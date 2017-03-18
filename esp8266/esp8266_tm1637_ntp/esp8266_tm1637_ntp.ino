0eef70391df217f9fdf6a99e530b236cf358962f
BrokenPen <acs103171@gm.ntcu.edu.tw>
2017-03-18 20:27:48 +0800

    updated
    Signed-off-by: BrokenPen <acs103171@gm.ntcu.edu.tw>

--- esp8266/esp8266_tm1637_ntp/esp8266_tm1637_ntp.ino
+++ esp8266/esp8266_tm1637_ntp/esp8266_tm1637_ntp.ino
@@ -9,8 +9,8 @@
 //#include <WiFi101.h> // for WiFi 101 shield or MKR1000
 #include <WiFiUdp.h>
 
-const char *ssid     = "your_ssid";
-const char *password = "your_password";
+const char *ssid     = "BROKENPEN-RPI";
+const char *password = "rpiBROKENPEN";
 
 WiFiUDP ntpUDP;
 
@@ -28,30 +28,67 @@ const byte PIN_CLK = 16; //D0   // define CLK pin (any digital pin)
 const byte PIN_DIO = 5; //D1  // define DIO pin (any digital pin)
 SevenSegmentExtended      display(PIN_CLK, PIN_DIO);
 
-// run setup code
-void setup() {
-  WiFi.begin(ssid, password);
-  Serial.begin(115200);         // initializes the Serial connection @ 9600 baud
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
-  }
+void setup_serial() {
+  Serial.begin(115200);         // initializes the Serial connection @ 115200 baud
+}
 
-  timeClient.begin();
-  
+void setup_display() {
   display.begin();            // initializes the display
   display.setBacklight(100);  // set the brightness to 100 %
+}
+
+
+void setup_wifi() {
+  if (WiFi.status() == WL_CONNECTED)
+    return;
+    
+  Serial.println(""); // a empty line
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println(""); // a empty line
+  Serial.println("WiFi connected");
+  Serial.println("IP address : ");
+  Serial.println(WiFi.localIP());
+}
+
+
+// run setup code
+void setup() {
+  setup_serial();
+  setup_wifi();
+
+  timeClient.begin(); // NTP begin
+  setup_display();
+ 
   delay(1000);                // wait 1000 ms
-};
+}
+
+
+void set_display(byte hours, byte minutes) {
+  display.printTime(hours, minutes, true);  // display time
+}
 
 // run loop (forever)
 void loop() {
-  String time_s = timeClient.getFormattedTime();
-  timeClient.update();
-  Serial.println(time_s); 
+  setup_wifi(); // if wifi disconnected, connect again
+  
+  String time_s = timeClient.getFormattedTime(); // get current time
+  timeClient.update(); // udpate ntp
+  Serial.println(time_s); // print current time
+  
   //getHours(); getMinutes(); also work in NTPClient..
-  byte hours    =  time_s.substring(0,2).toInt();                     // initialize hours
-  byte minutes  = time_s.substring(3,5).toInt();                   // initialize minutes
-  display.printTime(hours, minutes, true);  // display time
-  delay(1000);
+  byte hours    =  time_s.substring(0,2).toInt(); // initialize hours
+  byte minutes  = time_s.substring(3,5).toInt();  // initialize minutes
+  set_display(hours, minutes); // set content of TM1637 display
+  delay(1000); // delay 1second
 };
